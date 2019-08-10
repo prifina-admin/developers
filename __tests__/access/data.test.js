c70acc340ec938d448db89fef6e56ee03c7d5f0b
Syed Faraaz Ahmad <faraaz98@live.com>
2019-08-10 14:06:35 +0530

    define all test conditions

---
+++ __tests__/access/data.test.js
@@ -0,0 +1,21 @@
+// Tests for the data function
+
+const data = require('../../src/access').data;
+
+describe('Utility function \'request\'', () => {
+    test('Throws error if service is not registered in the database', () => {
+
+    });
+
+    test('Throws error if user is not registered in the database', () => {
+
+    });
+
+    test('throws error if user hasn\'t granted permission to service', () => {
+
+    });
+
+    test('Returns data if the request is valid', () => {
+
+    });
+});
--- __tests__/access/request.test.js
+++ __tests__/access/request.test.js
@@ -3,5 +3,19 @@
 const request = require('../../src/access').request;
 
 describe('Utility function \'request\'', () => {
+    test('Throws error if service is not registered in the database', () => {
+
+    });
+
+    test('Throws error if user is not registered in the database', () => {
+
+    });
     
+    test('Given valid input, should add the request to the database', () => {
+
+    });
+
+    test('Given invalid input, should not add the request to the database', () => {
+
+    });
 });
