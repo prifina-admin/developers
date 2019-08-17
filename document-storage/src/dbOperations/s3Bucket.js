3d996527c814d1eb93068e3779ffded77506f0ff
Mahantesha Junja <mahanteshj1996@gmail.com>
2019-08-17 15:20:39 +0530

    lambda-layer creation for the node_modules and changing return to callback function in lambda function

--- document-storage/src/dbOperations/s3Bucket.js
+++ document-storage/src/dbOperations/s3Bucket.js
@@ -1,8 +1,8 @@
 const AWS = require("aws-sdk");
 
 let s3 = new AWS.S3({
-  accessKeyId: "AKIAT2YRUHOLVIDYZ4VX", //process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: "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", //process.env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   s3BucketEndpoint: false,
   endpoint: "https://s3.amazonaws.com"
 });
