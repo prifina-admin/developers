9a2dbd63972c1cae2738ae289850c679a8f652c5
Tero <tero@starupcommons.org>
2020-07-21 13:00:49 +0200

    link

--- src/Link.js
+++ src/Link.js
@@ -1,27 +1,30 @@
 import React, { forwardRef } from "react";
 
 import styled from "styled-components";
-
 import { space } from "styled-system";
+import PropTypes from "prop-types";
 
 const LinkElement = styled.a.attrs(props => ({
   rel: props.target === "_blank" ? "noopener" : null,
 }))`
   cursor: pointer;
   text-decoration: none;
-  color: "currentColor";
+  color: currentColor;
 
   &:hover {
-    color: "currentColor";
+    color: ${props => props.theme.colors.textLink};
     text-decoration: underline;
   }
   ${space}
 `;
 
-const Link = forwardRef((props, ref) => {
-  return <LinkElement {...props} ref={ref} />;
+const Link = forwardRef(({ href, ...props }, ref) => {
+  return <LinkElement href={href} {...props} ref={ref} />;
 });
 
 Link.displayName = "Link";
-
+Link.propTypes = {
+  href: PropTypes.string.isRequired,
+  target: PropTypes.isRequired,
+};
 export default Link;
