d0da25a2890a8339983eb42d0dadbe233be973bd
Tero <tero@starupcommons.org>
2020-07-23 07:21:27 +0200

    icon field

--- src/IconField.js
+++ src/IconField.js
@@ -3,10 +3,11 @@ import Box from "./Box";
 
 import styled from "styled-components";
 import { space } from "styled-system";
-import PropTypes from "prop-types";
+//import PropTypes from "prop-types";
 import { useTheme } from "./theme/ThemeProvider";
 
 const StyledBox = styled(Box)`
+  ${space}
   display: flex;
   border: ${props =>
     typeof props.borders !== "undefined"
@@ -60,54 +61,3 @@ const IconField = props => {
 };
 
 export default IconField;
-
-/*
-return React.cloneElement(child, {
-      style: {
-        ...child.props.style,
-        paddingLeft: i === 0 ? undefined : 40,
-        paddingRight: i === children.length - 1 ? undefined : 40,
-      },
-    });
-import {
-  folderOutlineIcon as folderIcon,
-  dotsHorizontalIcon as settingsIcon,
-  viewAgendaOutlineIcon as modelsIcon,
-  BlendIcon,
-} from "@blend-ui/icons";
-import { I18n } from "../amplify/";
-
-const ListItemIconLink = ({ children, icon, onClick, ...props }) => (
-  <React.Fragment>
-    <BlendIcon iconify={icon} size={"20"} />
-    <Text ml={icon ? "14px" : 0} fontSize={"13px"} {...props}>
-      {children}
-    </Text>
-  </React.Fragment>
-);
-
-const FieldElement = styled.a.attrs(props => ({
-  rel: props.target === "_blank" ? "noopener" : null,
-}))`
-  cursor: pointer;
-  text-decoration: none;
-  color: currentColor;
-
-  &:hover {
-    color: ${props => props.theme.colors.textLink};
-    text-decoration: underline;
-  }
-  ${space}
-`;
-
-const IconField = forwardRef(({ href, ...props }, ref) => {
-  return <LinkElement href={href} {...props} ref={ref} />;
-});
-
-IconField.displayName = "Link";
-IconField.propTypes = {
-  href: PropTypes.string.isRequired,
-  target: PropTypes.string.isRequired,
-};
-export default IconField;
-*/
